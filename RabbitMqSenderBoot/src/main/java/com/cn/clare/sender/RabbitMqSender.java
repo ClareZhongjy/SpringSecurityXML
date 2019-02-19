b9a8751a35e5477e270ab1609183af7abf5ddb62
钟佳勇 <formssi-45@crc.hk.com>
2019-02-19 18:02:40 +0800

    设置失败回调方法

--- RabbitMqSenderBoot/src/main/java/com/cn/clare/sender/RabbitMqSender.java
+++ RabbitMqSenderBoot/src/main/java/com/cn/clare/sender/RabbitMqSender.java
@@ -2,6 +2,7 @@ package com.cn.clare.sender;
 
 import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate.ReturnCallback;
 import org.springframework.context.support.StaticApplicationContext;
@@ -19,6 +20,23 @@ public class RabbitMqSender {
 	public static RabbitMqSender InitMqSender() {
 		if(rabbitMqSender==null) {
 			final RabbitTemplate rabbitTemplate = SpringBeanUtil.getBeans(RabbitTemplate.class) ;
+
+			//如果ack=false，则是消息未到达exchange
+			rabbitTemplate.setConfirmCallback(
+					new RabbitTemplate.ConfirmCallback() {
+						@Override
+						public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+							if(!ack){
+								System.out.println("The message could deliver to exchange!"+cause+","+correlationData.toString());
+							}
+						}
+					}
+			);
+
+
+            //需设置mandatory=true,否则无论是否到达queue，均不回调
+            //exchange到queue成功，则不回调,失败则回调
+			rabbitTemplate.setMandatory(true);
 			rabbitTemplate.setReturnCallback(
 					new ReturnCallback() {
 						@Override
